9fd9703b0acd2f68f583a0757adab59d513499f3
vikas-koli <your-github-vikaskoli7400@gmail.com>
2025-11-03 13:36:52 +0530

    Initial commit

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,6 +1,3 @@
-module.exports = {
-    
+module.exports = { 
     url:"mongodb+srv://vikaskoli7400_db_user:wRc738ol2zNQCOBA@gasagencyproject.zbyt9zg.mongodb.net/?appName=GasAgencyProject"
-
 }
-    
